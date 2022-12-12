ba4553254912a5c4d448c4fdd0f6bb97f920392b
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-12-12 17:29:26 +0100

    replaced integers with bitmasks

--- src/types.hpp
+++ src/types.hpp
@@ -2,13 +2,13 @@
 #define __TYPES__
 
 // Bit maps of each color in the board
-typedef std::vector<std::pair<int, int> > t_board;
+typedef std::vector<std::pair<std::bitset<19>, std::bitset<19>> > t_board;
 
 // A vector containing the superposition of the bit masks
 typedef std::vector<int> t_super;
 
 // A pair representing the coordinants of a board position
-typedef std::pair<int, int> t_coords;
+typedef std::pair<int, int> t_coord;
 
 // A vector of children generated form a board
 typedef std::vector<std::pair<int, int> > t_children;
