b457acecc0a3d3f42c2cd12dbf5cf22cd83d8bda
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-12-12 18:15:46 +0100

    added get_possible_coords

--- src/minimax.cpp
+++ src/minimax.cpp
@@ -1,41 +1,59 @@
 #include "minimax.hpp"
+#include <bitset>
 
 #define COORDS(result) (result.first)
 #define SCORE(result) (result.second)
+#define GET_BIT(bitset, index) (bitset[18 + index])
 
 
-/* Combine two bit masks into one */
 t_super superpose(t_board &board)
 {
     t_super superposition;
 
-    for (t_super::iterator it = board.begin(); it != it.end(); it++)
+    for (t_board::iterator row = board.begin(); row != board.end(); row++)
     {
-        superposition.push_back((*it).first | (*it).second);
+        superposition.push_back((*row).first.to_ulong() | (*row).second.to_ulong());
     }
     return superposition;
 }
 
 
-std::vector<t_coord> get_possible_coords(t_board)
+std::vector<t_coord> get_possible_coords(t_board &board, int player)
 {
-    
+    std::vector<t_coord> coords;
+
+    if (player == 1)
+    {
+        for (int i = 0; i <  board.size(); i++)
+        {
+
+        }
+    }
+    else
+    {
+        for (t_board::iterator row = board.begin(); it != board.end(); it++)
+        {
+
+        }
+    }
+    return coords;
 }
 
 
-t_board update_board(t_board &board, t_coord coord, int turn)
+t_board update_board(t_board &board, t_coord &coord, int player)
 {
     t_board new_board;
-    std::bitset<19> &mask;
 
     new_board = board;
-    mask = turn ? new_board[coord.first].first : new_board[coord.first].second;
-    mask.set(coord.second, 1);
+    if (player == 1)
+        new_board[coord.first].first.set(coord.second, 1);
+    else
+        new_board[coord.forst].second.set(coord.second, 1);
     return new_board;
 }
 
 
-std::pair<t_coord, int> maximize(int depth, t_board &board, int &alpha, int &beta, int turn)
+std::pair<t_coord, int> maximize(int depth, t_board &board, int &alpha, int &beta, int player)
 {
     std::pair<std::pair<int, int>, int> result;
     int best_index;
@@ -47,67 +65,50 @@ std::pair<t_coord, int> maximize(int depth, t_board &board, int &alpha, int &bet
 
     for (int i = 0; i < children.size(); i++)
     {
-        // run minimax on the current child as a minimizer
-        result = minimax(depth - 1, update_board(children[i]), alpha, beta, false);
-
-        // proun the rest of the children
+        result = minimax(depth - 1, update_board(board, children[i]), alpha, beta, false);
         alpha = std::max(alpha, SCORE(result));
         if (beta <= alpha)
             break ;
-
-        // update index and score of the best
         best_index = i;
         best_score = std::max(SCORE(result), best_score);
     }
-
-    // return the best coordinates and score
     result.first = children[best_index];
     result.second = best_score;
     return result;
 }
 
 
-
-
-std::pair<std::pair<int, int>, int> minimize(int depth, t_board &board, int &alpha, int &beta, int turn)
+std::pair<t_coord, int> minimize(int depth, t_board &board, int &alpha, int &beta, int player)
 {
-    std::pair<std::pair<int, int>, int> result;
+    std::pair<t_coord, int> result;
     int best_index;
     int best_score;
 
-    children = get_possible_coords(board);
+    children = get_possible_coords(board, player);
     best_score = INT32_MAX;
     best_index = 0;
 
     for (int i = 0; i < children.size(); i++)
     {
-        // run minimax on the current child as a minimizer
-        result = minimax(depth - 1, update_board(children[i]), alpha, beta, true);
-
-        // proun the rest of the children
+        result = minimax(depth - 1, update_board(board, children[i]), alpha, beta, true);
         beta = std::min(beta, SCORE(result));
         if (beta <= alpha)
             break ;
-
-        // update index and score of the best
         best_index = i;
         best_score = std::min(SCORE(result), best_score);
     }
 
-    // return the best coordinates and score
     result.first = children[best_index];
     result.second = best_score;
     return result;
 }
 
 
-std::pair<std::pair<int, int>, int> minimax(int depth, t_board &board, int &alpha, int &beta, int is_max, int turn)
+std::pair<std::pair<int, int>, int> minimax(int depth, t_board &board, int &alpha, int &beta, int is_max, int player)
 {
     if (depth == 0)
         return static_evaluation(board);
-
     if (is_max)
-        return maximize(depth, board, alpha, beta);
-    int i = 0;
-    return minimize(depth, board, alpha, beta);
+        return maximize(depth, board, alpha, beta, player);
+    return minimize(depth, board, alpha, beta, player);
 }
