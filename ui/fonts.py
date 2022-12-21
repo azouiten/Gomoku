dbdc0a4ace92d65c680b18c4d56b2ce67793845b
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-12-21 19:27:09 +0100

    moved fonts constants to fonts.py

---
+++ ui/fonts.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+# Load fonts
+h1_b = pygame.font.Font('./ressources/fonts/ChivoMono-Bold.ttf', 96)
+h2_b = pygame.font.Font('./ressources/fonts/ChivoMono-Bold.ttf', 56)
+h3_b = pygame.font.Font('./ressources/fonts/ChivoMono-Bold.ttf', 32)
+h1_r = pygame.font.Font('./ressources/fonts/ChivoMono-Regular.ttf', 96)
+h2_r = pygame.font.Font('./ressources/fonts/ChivoMono-Regular.ttf', 56)
+h3_r = pygame.font.Font('./ressources/fonts/ChivoMono-Regular.ttf', 32)
+h1_t = pygame.font.Font('./ressources/fonts/ChivoMono-Thin.ttf', 96)
+h2_t = pygame.font.Font('./ressources/fonts/ChivoMono-Thin.ttf', 56)
+h3_t = pygame.font.Font('./ressources/fonts/ChivoMono-Thin.ttf', 32)
