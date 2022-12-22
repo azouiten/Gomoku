7ec733abce9da2925a15e1671c337df0a8624a28
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-12-22 15:16:03 +0100

    removed image board rendering and replaced with lines rendering with hover position

--- ui/visualizer.py
+++ ui/visualizer.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from surface import Surface
 from button import Button
 from fonts import *
@@ -63,19 +64,24 @@ class Board(Surface):
     """
     def __init__(self, window, player, initial_state):
         super().__init__(WIDTH, HEIGHT)
-        self._offset  = 50
         self._player  = player
-        self._board   = Surface(HEIGHT - self._offset*2, HEIGHT - self._offset*2)
+        self._board   = Surface(HEIGHT, HEIGHT)
         self._sidebar = Surface(WIDTH-HEIGHT, HEIGHT)
         self._state   = initial_state
-        self._image   = pygame.image.load('./ressources/images/go-board.png')
-        self._image   = pygame.transform.scale(self._image, (self._board.height+5, self.board.height+5))
         self._window  = window
         self.repeat   = True
+        self.offset   = 40
+        self.limit    = self.board.height - self.offset * 2 - 1
+        self.step     = int(self.limit / 18)
+        self.linspace = [i+self.step for i in range(0, self.board.width - self.offset, self.step)]
 
     @property
     def player(self):
         return self._player
+
+    @player.setter
+    def player(self, value):
+        self._player = value
 
     @property
     def board(self):
@@ -88,21 +94,63 @@ class Board(Surface):
     @property
     def window(self):
         return self._window
+
+    def draw_board(self):
+        self.board.surface.fill(pygame.Color("#E6C475"))
+        for i in range(0, 19):
+            thickness = 3 if i in (0, 18) else 2 if i in (3, 9, 15) else 1
+
+            if i in (3, 9, 15):
+                for j in (3, 9, 15):
+                    x = self.offset + j * self.step + 1
+                    y = self.offset + i * self.step + 1
+                    pygame.draw.circle(self.board.surface, pygame.Color('#000000'), (x, y), 5)
+
+            # Draw horizontal lines
+            xs = self.offset + 0
+            ys = self.offset + i * self.step
+            xe = self.offset + self.limit
+            ye = self.offset + i * self.step
+            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), thickness)
+
+            # Draw vertical lines
+            xs = self.offset + i * self.step
+            ys = self.offset + 0
+            xe = self.offset + i * self.step
+            ye = self.offset + self.limit
+            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), thickness)
         
     def update_board(self):
-        self.board.surface.fill(pygame.Color("#E6C475"))
-        self.board.surface.blit(self._image, (-2, -2))
-        self.surface.blit(self.board.surface, (self._offset, self._offset))
+        self.draw_board()
+        if self.check_hover():
+            self.show_hover()
+        self.surface.blit(self.board.surface, (0, 0))
 
     def update_sidebar(self):
         self.sidebar.surface.fill(pygame.Color("#EAE6E3"))
-        self.surface.blit(self.sidebar.surface, (self.board.width + self._offset * 2, 0))
+        self.surface.blit(self.sidebar.surface, (self.board.width, 0))
 
     def update(self):
         self.update_board()
         self.update_sidebar()
         self.window.blit(self)
-        pygame.display.update()
+        self.window.update()
+
+    def check_hover(self):
+        x, y = pygame.mouse.get_pos()
+        if x >= self.offset and x <= self.board.height - self.offset:
+            if y >= self.offset and y <= self.board.height - self.offset:
+                return True
+        return False
+
+    def show_hover(self):
+        x, y = pygame.mouse.get_pos()
+        color = "#000000" if self.player == 2 else "#ffffff"
+        x = self.linspace[math.floor((x-20) / self.step)] - 10
+        y = self.linspace[math.floor((y-20) / self.step)] - 10
+        pygame.draw.circle(self.board.surface, pygame.Color(color), (x, y), 20)
+        return x, y
+        
 
     def loop(self):
         global QUIT
@@ -114,6 +162,8 @@ class Board(Surface):
                     QUIT = True
                     self.repeat = False
                     continue
+                if event.type == pygame.MOUSEBUTTONUP and self.check_hover():
+                    self.player = 1 if self.player == 2 else 2
             self.update()
 
 
@@ -145,8 +195,8 @@ class Setup(Surface):
 
         header = h3_t.render('Player 1', True, pygame.Color('#000000'), pygame.Color("#ffffff"))
         header_rect = header.get_rect()
-        header_rect.center = (self.player_1.rect.center[0] / 2, self.player_1.rect.center[1] / 2)
-        
+        header_rect.center = (self.player_1.rect.center[0] / 2, self.player_1.rect.center[1] / 2 - 200)
+
         self.player_1.surface.fill(WHITE)
         self.player_1.surface.blit(header, header_rect)
         self.surface.blit(self.player_1.surface, self.player_1.rect)
@@ -254,7 +304,6 @@ class Final(Surface):
             self.surface.blit(self.button.surface, self.button.rect)
             self.window.blit(self)
             pygame.display.update()
-            
 
 
 class Game:
@@ -268,7 +317,7 @@ class Game:
         self._setup_surface = Setup(self._window)
         self._board_surface = Board(self._window, 1, self._state)
         self._final_surface = Final(self._window)
-        self._current_surface = 3
+        self._current_surface = 2
 
 
     @property
