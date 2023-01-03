0f817f84f9fdbf0428c88f6b06e96c0d76fcdced
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-03 14:11:19 +0100

    check first box

--- ui/visual_components.py
+++ ui/visual_components.py
@@ -175,7 +175,7 @@ class CheckBoxs(Surface):
 
         # Anchor to first element of container list or set to None
         self._anchor = self._container[0] if self._container else None
-
+        self._anchor.checked = True
         super().__init__(total_width, offset, (position[0]+100, position[1]))
         self.rect.top += 100
 
