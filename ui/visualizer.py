c280c320e295ed157f9e044e796b01b8c311ac1f
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-03 16:52:11 +0100

    reorder classes

--- ui/visualizer.py
+++ ui/visualizer.py
@@ -92,160 +92,6 @@ class Window:
         pygame.display.flip()
 
 
-class Board(Surface):
-    """ 
-    This class represents the board surface (game board + sidebar).
-    """
-    __slots__ = ('_player', '_board', '_sidebar', '_state', '_window', '_repeat', '_offset', '_limit', '_step', '_linspace')
-    def __init__(self, window, player, initial_state):
-        super().__init__(WIDTH, HEIGHT, (0, 0))
-        self._player  = player
-        self._board   = Surface(HEIGHT, HEIGHT, None)
-        self._sidebar = Surface(WIDTH-HEIGHT, HEIGHT, None)
-        self._state   = initial_state
-        self._window  = window
-        self._repeat   = True
-        self._offset   = 40
-        self._limit    = self.board.height - self.offset * 2 - 18
-        self._step     = int(self.limit / 18)
-        self._linspace = [i+self.step for i in range(0, self.board.width - self.offset, self.step)]
-
-    @property
-    def player(self):
-        return self._player
-
-    @player.setter
-    def player(self, value):
-        self._player = value
-
-    @property
-    def board(self):
-        return self._board
-
-    @property
-    def sidebar(self):
-        return self._sidebar
-
-    @property
-    def state(self):
-        return self._state
-
-    @state.setter
-    def state(self, value):
-        self._state = value
-
-    @property
-    def repeat(self):
-        return self._repeat
-
-    @repeat.setter
-    def repeat(self, value):
-        self._repeat = value
-
-    @property
-    def offset(self):
-        return self._offset
-
-    @property
-    def limit(self):
-        return self._limit
-
-    @property
-    def step(self):
-        return self._step
-
-    @property
-    def linspace(self):
-        return self._linspace
-
-    @property
-    def window(self):
-        return self._window
-
-    def draw_board(self):
-        self.board.surface.fill(BOARD_COLOR)
-
-        for i in range(0, 19):
-            # Draw horizontal lines
-            xs = self.offset + 0
-            ys = self.offset + i * self.step
-            xe = self.offset + self.limit
-            ye = self.offset + i * self.step
-            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), 1)
-
-            # Draw vertical lines
-            xs = self.offset + i * self.step
-            ys = self.offset + 0
-            xe = self.offset + i * self.step
-            ye = self.offset + self.limit
-            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), 1)
-
-    def draw_state(self):
-        for r, row in enumerate(self.state.state):
-            for c, col in enumerate(row):
-                if col in ['1', '2']:
-                    color = "#ffffff" if col == '1' else "#000000"
-                    x = self.linspace[c] + 1
-                    y = self.linspace[r] + 1
-                    draw_circle(self.board.surface, x, y, 16, pygame.Color(color))
-
-    def check_hover(self):
-        x, y = pygame.mouse.get_pos()
-        if x >= self.offset and x <= self.limit + self.offset:
-            if y >= self.offset and y <= self.limit + self.offset:
-                return True
-        return False
-
-    def show_hover(self):
-        radius = 20
-        x, y = pygame.mouse.get_pos()
-        color = "#ffffff" if self.player == 1 else "#000000"
-        nx = self.linspace[math.floor((x-radius) / self.step)]
-        ny = self.linspace[math.floor((y-radius) / self.step)]
-        if math.sqrt((x-nx)**2 + (y-ny)**2) <= 25:
-            x, y = nx + 1, ny + 1
-        draw_circle(self.board.surface, x, y, radius, pygame.Color(color))
-        return x, y
-
-    def update_board(self):
-        self.draw_board()
-        self.draw_state()
-        if self.check_hover():
-            self.show_hover()
-        self.surface.blit(self.board.surface, (0, 0))
-
-    def update_sidebar(self):
-        self.sidebar.surface.fill(pygame.Color("#EAE6E3"))
-        self.surface.blit(self.sidebar.surface, (self.board.width, 0))
-
-    def update(self):
-        self.update_board()
-        self.update_sidebar()
-        self.window.blit(self)
-        self.window.update()
-
-    def loop(self):
-        global QUIT
-
-        self.update()
-        while self.repeat:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    QUIT = True
-                    self.repeat = False
-                    break
-                if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.check_hover():
-                    x, y = pygame.mouse.get_pos()
-                    x = math.floor((x-16) / self.step)
-                    y = math.floor((y-16) / self.step)
-                    print(x, y, self.state.state[y][x])
-                    if self.state.state[y][x] == '0':
-                        self.state.update(x, y, str(self.player))
-                        self.player = 1 if self.player == 2 else 2
-            if not QUIT:
-                self.update()
-
-
 class Setup(Surface):
     """ 
     This class represents the setup surface
@@ -414,6 +260,160 @@ class Setup(Surface):
             self.draw_box_2()
             self.window.blit(self)
             pygame.display.update()
+
+
+class Board(Surface):
+    """ 
+    This class represents the board surface (game board + sidebar).
+    """
+    __slots__ = ('_player', '_board', '_sidebar', '_state', '_window', '_repeat', '_offset', '_limit', '_step', '_linspace')
+    def __init__(self, window, player, initial_state):
+        super().__init__(WIDTH, HEIGHT, (0, 0))
+        self._player  = player
+        self._board   = Surface(HEIGHT, HEIGHT, None)
+        self._sidebar = Surface(WIDTH-HEIGHT, HEIGHT, None)
+        self._state   = initial_state
+        self._window  = window
+        self._repeat   = True
+        self._offset   = 40
+        self._limit    = self.board.height - self.offset * 2 - 18
+        self._step     = int(self.limit / 18)
+        self._linspace = [i+self.step for i in range(0, self.board.width - self.offset, self.step)]
+
+    @property
+    def player(self):
+        return self._player
+
+    @player.setter
+    def player(self, value):
+        self._player = value
+
+    @property
+    def board(self):
+        return self._board
+
+    @property
+    def sidebar(self):
+        return self._sidebar
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+
+    @property
+    def repeat(self):
+        return self._repeat
+
+    @repeat.setter
+    def repeat(self, value):
+        self._repeat = value
+
+    @property
+    def offset(self):
+        return self._offset
+
+    @property
+    def limit(self):
+        return self._limit
+
+    @property
+    def step(self):
+        return self._step
+
+    @property
+    def linspace(self):
+        return self._linspace
+
+    @property
+    def window(self):
+        return self._window
+
+    def draw_board(self):
+        self.board.surface.fill(BOARD_COLOR)
+
+        for i in range(0, 19):
+            # Draw horizontal lines
+            xs = self.offset + 0
+            ys = self.offset + i * self.step
+            xe = self.offset + self.limit
+            ye = self.offset + i * self.step
+            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), 1)
+
+            # Draw vertical lines
+            xs = self.offset + i * self.step
+            ys = self.offset + 0
+            xe = self.offset + i * self.step
+            ye = self.offset + self.limit
+            pygame.draw.line(self.board.surface, pygame.Color('#000000'), (xs, ys), (xe, ye), 1)
+
+    def draw_state(self):
+        for r, row in enumerate(self.state.state):
+            for c, col in enumerate(row):
+                if col in ['1', '2']:
+                    color = "#ffffff" if col == '1' else "#000000"
+                    x = self.linspace[c] + 1
+                    y = self.linspace[r] + 1
+                    draw_circle(self.board.surface, x, y, 16, pygame.Color(color))
+
+    def check_hover(self):
+        x, y = pygame.mouse.get_pos()
+        if x >= self.offset and x <= self.limit + self.offset:
+            if y >= self.offset and y <= self.limit + self.offset:
+                return True
+        return False
+
+    def show_hover(self):
+        radius = 20
+        x, y = pygame.mouse.get_pos()
+        color = "#ffffff" if self.player == 1 else "#000000"
+        nx = self.linspace[math.floor((x-radius) / self.step)]
+        ny = self.linspace[math.floor((y-radius) / self.step)]
+        if math.sqrt((x-nx)**2 + (y-ny)**2) <= 25:
+            x, y = nx + 1, ny + 1
+        draw_circle(self.board.surface, x, y, radius, pygame.Color(color))
+        return x, y
+
+    def update_board(self):
+        self.draw_board()
+        self.draw_state()
+        if self.check_hover():
+            self.show_hover()
+        self.surface.blit(self.board.surface, (0, 0))
+
+    def update_sidebar(self):
+        self.sidebar.surface.fill(pygame.Color("#EAE6E3"))
+        self.surface.blit(self.sidebar.surface, (self.board.width, 0))
+
+    def update(self):
+        self.update_board()
+        self.update_sidebar()
+        self.window.blit(self)
+        self.window.update()
+
+    def loop(self):
+        global QUIT
+
+        self.update()
+        while self.repeat:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    QUIT = True
+                    self.repeat = False
+                    break
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1 and self.check_hover():
+                    x, y = pygame.mouse.get_pos()
+                    x = math.floor((x-16) / self.step)
+                    y = math.floor((y-16) / self.step)
+                    print(x, y, self.state.state[y][x])
+                    if self.state.state[y][x] == '0':
+                        self.state.update(x, y, str(self.player))
+                        self.player = 1 if self.player == 2 else 2
+            if not QUIT:
+                self.update()
 
 
 class Final(Surface):
